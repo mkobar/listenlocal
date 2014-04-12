2434eec12a47017ccd18771a42faa29e19e58b39
Eric Austin <xxfendericxx@gmail.com>
2014-04-12 10:59:10 -0500

    changing bottom player bar

--- simple-player/js/main.js
+++ simple-player/js/main.js
@@ -57,9 +57,12 @@
         
         R.player.on("change:playingTrack", function(track) {
           $(".header .icon").attr("src", track.get("icon"));
-          $(".header .track").text("Track: " + track.get("name"));
-          $(".header .album-title").text("Album: " + track.get("album"));
-          $(".header .artist").text("Artist: " + track.get("artist"));
+          $(".header .track").text(track.get("name"));
+          $(".header .album-title").text(track.get("album"));
+          $(".header .artist").text(track.get("artist"));
+          //$(".header .track").text("Track: " + track.get("name"));
+          //$(".header .album-title").text("Album: " + track.get("album"));
+          //$(".header .artist").text("Artist: " + track.get("artist"));
         });
         
         R.player.on("change:playState", function(state) {
